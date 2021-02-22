d016ddfbd169e5efb6928ce1361001138b611d34
Uğur <ugur.mamak@portalgrup.com>
2021-02-22 14:34:41 +0300

    react-html-parser important

--- pages/task-detail.js
+++ pages/task-detail.js
@@ -3,14 +3,14 @@ import Link from "next/link"
 import "suneditor/dist/css/suneditor.min.css";
 import SunEditor, {buttonList} from "suneditor-react";
 import {SRLWrapper} from "simple-react-lightbox";
-
 import Select from "react-select";
 
 const progressTypeData = [
-    { value: '1', label: 'Görev Atandı(to do)' },
-    { value: '2', label: 'Yapım Aşamasında (inProgress)' },
-    { value: '3', label: 'Tamamlandı (done)' }
+    {value: '1', label: 'Görev Atandı(to do)'},
+    {value: '2', label: 'Yapım Aşamasında (inProgress)'},
+    {value: '3', label: 'Tamamlandı (done)'}
 ]
+
 
 class TaskDetail extends Component {
 
@@ -18,13 +18,19 @@ class TaskDetail extends Component {
         super(props);
         this.editorRef = React.createRef();
         this.state = {
-            toggle: false
+            toggle: false,
         }
     }
 
     componentDidMount() {
-        console.log(this.editorRef.current.editor);
-        // this.editorRef.current.editor.hide();
+
+        //dropdown açılınca menu'nün ekran dışına taşmasını önlemek için ekledim.
+        $('.comment-options').on('show.bs.dropdown',function (){
+            const $that=$(this);
+            if($(window).width()<=320){
+                $that.find('.dropdown-menu').addClass('dropdown-menu-right');
+            }
+        });
     }
 
     addComment = (e) => {
@@ -53,27 +59,34 @@ class TaskDetail extends Component {
         e.preventDefault();
         const editor = this.editorRef.current.editor;
 
-        console.log(editor);
-        console.log(editor.getContext().element);
+        console.log("editor", editor);
+        console.log("getContext", editor.getContext().element);
+
+        console.log("getContets", editor.getContents(false));
+        console.log("save", editor.save());
 
         // console.log(editor.getContents()); //html data
         // console.log(editor.getImagesInfo()); //img info
 
-        //içerideki img ve textleri parçala
-        // img'lere alt bilgisini zorunlu hale getir
-
     }
+
+
+
+
+    onScroll=(e,core)=>{
+    console.log(e);
+    console.log(core);
+    }
+
 
     render() {
 
         return (
             <>
-                <h1>Kullanıcıların task içeriğini görebileceği yorum yazabileceği ve task ilerleme durumlarını
-                    belirtebileceği sayfa olacak</h1>
                 <div className="container">
                     <div className="task-detail-wrapper">
                         <div className="row">
-                            <div className="col-lg-8">
+                            <div className="col-lg-8 col-24">
                                 <div className="task-content">
                                     <div className="header">
                                         <img className="img-fluid d-flex user-img"
@@ -152,17 +165,17 @@ class TaskDetail extends Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-lg-4">
+                            <div className="col-lg-4 col-24">
                                 <div className="task-operation">
                                     <ul>
                                         <li>
                                             <h5 className="label">Atayan</h5>
-                                            <Link href="/"><a>Deneme denem</a></Link>
+                                            <Link href="/"><a>Uğur Mamak</a></Link>
                                         </li>
                                         <li>
                                             <h5 className="label">Progress</h5>
                                             <Select
-                                                className="progress-type-select"
+                                                className="primary-select-container"
                                                 options={progressTypeData}
                                                 classNamePrefix="select"
                                                 instanceId="progress-type"
@@ -190,7 +203,6 @@ class TaskDetail extends Component {
                                     <button className="btn" onClick={this.addComment}>Yorum Ekle</button>
                                     <SunEditor
                                         ref={this.editorRef}
-                                        autoFocus={true}
                                         lang="en"
                                         name="editor-deneme"
                                         setOptions={{
@@ -199,7 +211,6 @@ class TaskDetail extends Component {
                                             placeholder: "Enter your text here!!!",
                                             buttonList: [
                                                 ["undo", "redo"],
-                                                ["font", "fontSize", "formatBlock"],
                                                 ["paragraphStyle"],
                                                 [
                                                     "bold",
@@ -234,8 +245,8 @@ class TaskDetail extends Component {
                                             ]
                                         }}
                                         onChange={this.handleChange}
-                                        onImageUpload={this.handleImageUpload}
-                                        className="deneme"
+                                        onScroll={this.onScroll}
+                                        hide={true}
                                     />
                                     <button className="btn btn-primary" onClick={this.save}>Kaydet</button>
                                 </div>
@@ -261,7 +272,7 @@ class TaskDetail extends Component {
                                                             aria-expanded="false">
                                                         <i className="icon icon-more"></i>
                                                     </button>
-                                                    <div className="dropdown-menu dropdown-menu-right"
+                                                    <div className="dropdown-menu"
                                                          aria-labelledby="dropdownMenuButton">
                                                         <a className="dropdown-item" href="/">Sil</a>
                                                         <a className="dropdown-item" href="/">Düzenle</a>

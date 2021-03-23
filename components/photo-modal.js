98dca28911bc9c6118b3dd610f74dcdd7f8a3023
UgurMamak <ugurmamak98@gmail.com>
2021-03-23 15:20:41 +0300

    photo-modal updated

--- components/photo-modal.js
+++ components/photo-modal.js
@@ -7,6 +7,14 @@ import 'react-image-crop/dist/ReactCrop.css';
 class PhotoModal extends Component {
 
     state = {
+        user: {
+            email: "ugurmamak98@gmail.com",
+            firstName: "uğur",
+            lastName: "mamak",
+            img: "https://bootdey.com/img/Content/avatar/avatar7.png"
+        },
+        photoModalStatus: false,
+        src: null,
         crop: {
             unit: 'px',
             width: 300,
@@ -15,35 +23,166 @@ class PhotoModal extends Component {
             minHeight: 300,
             locked: true,
             disabled: true
+        },
+        imgInfo: [],
+        file: "",
+        croppedImageUrl: "" //kırpılmış img'i göstermek için
+    }
+
+
+    handleImgChange = (event) => {
+
+        let files = event.target.files,
+            file,
+            fileType,
+            validImageTypes = ["image/gif", "image/jpeg", "image/png"],
+            done = (url) => {
+                this.setState({src: url, file: file, photoModalStatus: true});
+            };
+
+        if (event.target.files && event.target.files.length > 0) {
+            file = files[0];
+            fileType = file["type"];
+            if ($.inArray(fileType, validImageTypes) > 0) {
+                if (URL) {
+                    done(window.URL.createObjectURL(file));
+                } else if (FileReader) {
+                    var reader = new FileReader();
+                    reader.onloadend = function (e) {
+                        done(reader.result);
+                    }.bind(this);
+                    reader.readAsDataURL(file);
+                }
+            } else {
+                console.log("hata");
+            }
         }
     }
 
+    onImageLoaded = image => {
+        this.imageRef = image;
+    };
+    onCropChange = (crop, percentCrop) => {
+        this.setState({crop});
+    };
+
+    async makeClientCrop(crop) {
+        if (this.imageRef && crop.width && crop.height) {
+            const croppedImageUrl = await this.getCroppedImg(
+                this.imageRef,
+                crop,
+                'newFile.jpeg'
+            );
+            this.setState({croppedImageUrl});
+        }
+    }
+
+    //kırpm işlemi
+    getCroppedImg(image, crop, fileName) {
+        const canvas = document.createElement('canvas');
+        const scaleX = image.naturalWidth / image.width;
+        const scaleY = image.naturalHeight / image.height;
+        canvas.width = crop.width;
+        canvas.height = crop.height;
+        const ctx = canvas.getContext('2d');
+
+
+        ctx.drawImage(
+            image,
+            crop.x * scaleX,
+            crop.y * scaleY,
+            crop.width * scaleX,
+            crop.height * scaleY,
+            0,
+            0,
+            crop.width,
+            crop.height
+        );
+
+        return new Promise((resolve, reject) => {
+            canvas.toBlob(blob => {
+                if (!blob) {
+                    //reject(new Error('Canvas is empty'));
+                    console.error('Canvas is empty');
+                    return;
+                }
+                blob.name = this.state.file.name;
+                window.URL.revokeObjectURL(this.fileUrl);
+                this.fileUrl = window.URL.createObjectURL(blob);
+                this.state.imgInfo.push({
+                    fileName: this.state.file.name,
+                    value: blob
+                });
+                resolve(this.fileUrl);
+            }, 'image/jpeg');
+        });
+    }
+
+    //modal close
+    handleClose = () => {
+        this.setState({photoModalStatus: false})
+    };
+
+    saveCrop = (crop) => {
+        this.makeClientCrop(this.state.crop);
+        this.setState({
+            photoModalStatus: false
+        });
+    }
+
     render() {
+        const {crop,src, croppedImageUrl,  croppedImg, photoModalStatus} = this.state;
+
         return (
-            <Modal className="photo-modal" data-toggle="photo-modal" show={this.state.photoModalStatus} onHide={this.handleClose} centered>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    {src && (
-                        <ReactCrop
-                            src={src}
-                            crop={crop}
-                            ruleOfThirds
-                            onImageLoaded={this.onImageLoaded}
-                            onChange={this.onCropChange}
-                        />
-                    )}
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={this.handleClose}>
-                        İptal
-                    </Button>
-                    <Button variant="primary" onClick={this.saveCrop}>
-                        Kaydet
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+            <>
+                    <div className="photo-upload-widget-wrapper">
+                        <label>
+                            <input className="file-upload-input required d-none" type="file" id="profile-photo"
+                                   name="primaryPhoto"
+                                   accept=".png, .jpg, .jpeg"
+                                   onChange={this.handleImgChange}
+                                   data-msg-required="Bu alan boş bırakılamaz."/>
+                            <div className="photo-upload-widget" data-toggle="img-content">
+                                {croppedImageUrl === "" ?
+                                    <div data-toggle="cropedImg" className="img"
+                                         style={{backgroundImage: `url(${this.state.user.img})`}}></div>
+                                    : <div data-toggle="cropedImg" className="img"
+                                           style={{backgroundImage: `url(${croppedImageUrl})`}}></div>
+                                }
+                                <div className="upload-content" data-toggle="triggerUpload">
+                                    <i className="icon icon-add-picture"></i>
+                                    <span className="text">Fotoğraf Yükle</span>
+                                </div>
+                            </div>
+                        </label>
+                    </div>
+                    <button type="submit" onClick={()=>{this.props.imgSave(this.state.imgInfo)}} className="btn btn-primary">Kaydet</button>
+
+                <Modal className="photo-modal" data-toggle="photo-modal" show={photoModalStatus} onHide={this.handleClose} centered>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {src && (
+                            <ReactCrop
+                                src={src}
+                                crop={crop}
+                                ruleOfThirds
+                                onImageLoaded={this.onImageLoaded}
+                                onChange={this.onCropChange}
+                            />
+                        )}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            İptal
+                        </Button>
+                        <Button variant="primary" onClick={this.saveCrop}>
+                            Kaydet
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
         );
     }
 }
--- pages/profile.js
+++ pages/profile.js
@@ -5,6 +5,8 @@ import Modal from "react-bootstrap/Modal";
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
 
+import PhotoModal from "../components/photo-modal";
+
 class Profile extends Component {
     state = {
         user: {
@@ -13,146 +15,26 @@ class Profile extends Component {
             lastName: "mamak",
             img: "https://bootdey.com/img/Content/avatar/avatar7.png"
         },
-        photoModalStatus: false,
-        src: null,
-        crop: {
-            unit: 'px',
-            width: 300,
-            aspect: 16 / 9,
-            minWidth:300,
-            minHeight:300,
-            locked:true,
-            disabled:true
-        },
-        imgInfo: [],
-        file: "",
-        croppedImageUrl: ""
     }
 
-    handleImgSubmit = () => {
+    //child component'ten gelen data
+    imgSave=(data)=>{
+        console.log("kaydeldi");
+        console.log(data);
     }
 
-    handleImgChange = (event) => {
-        if (event.target.files && event.target.files.length > 0) {
-            const file = event.target.files[0];
-            var reader = new FileReader();
-            var url = reader.readAsDataURL(file);
-            reader.onloadend = function (e) {
-                // this.setState({src: reader.result})
-                this.setState({src: window.URL.createObjectURL(file), file: file});
-            }.bind(this);
-        } else {
-            console.log("boş var");
-        }
-
-        this.setState({photoModalStatus: true});
-    }
-
-
-//******
-    onImageLoaded = image => {
-        this.imageRef = image;
-    };
-    onCropChange = (crop, percentCrop) => {
-        this.setState({crop});
-    };
-
-//****
-
-
-    async makeClientCrop(crop) {
-        if (this.imageRef && crop.width && crop.height) {
-            const croppedImageUrl = await this.getCroppedImg(
-                this.imageRef,
-                crop,
-                'newFile.jpeg'
-            );
-            this.setState({croppedImageUrl});
-        }
-    }
-
-    //kırpm işlemi
-    getCroppedImg(image, crop, fileName) {
-        const canvas = document.createElement('canvas');
-        const scaleX = image.naturalWidth / image.width;
-        const scaleY = image.naturalHeight / image.height;
-        canvas.width = crop.width;
-        canvas.height = crop.height;
-        const ctx = canvas.getContext('2d');
-
-
-        ctx.drawImage(
-            image,
-            crop.x * scaleX,
-            crop.y * scaleY,
-            crop.width * scaleX,
-            crop.height * scaleY,
-            0,
-            0,
-            crop.width,
-            crop.height
-        );
-
-        return new Promise((resolve, reject) => {
-            canvas.toBlob(blob => {
-                if (!blob) {
-                    //reject(new Error('Canvas is empty'));
-                    console.error('Canvas is empty');
-                    return;
-                }
-                blob.name = this.state.file.name;
-                window.URL.revokeObjectURL(this.fileUrl);
-                this.fileUrl = window.URL.createObjectURL(blob);
-                this.state.imgInfo.push({
-                    fileName: this.state.file.name,
-                    value: blob
-                });
-                resolve(this.fileUrl);
-            }, 'image/jpeg');
-        });
-    }
-
-    //modal close
-    handleClose = () => {
-        this.setState({photoModalStatus: false})
-    };
-
-    saveCrop = (crop) => {
-        this.makeClientCrop(this.state.crop);
-        this.setState({
-            photoModalStatus: false
-        });
+    handleImgSubmit=(e)=>{
+        e.preventDefault();
     }
 
     render() {
-        const {crop, croppedImageUrl, src, croppedImg} = this.state;
         return (
             <Layout>
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-3 col-24">
                             <form action="" onSubmit={this.handleImgSubmit}>
-                                <div className="photo-upload-widget-wrapper">
-                                    <label>
-                                        <input className="file-upload-input required d-none" type="file" id="profile-photo"
-                                               name="primaryPhoto"
-                                               accept=".png, .jpg, .jpeg"
-                                               onChange={this.handleImgChange}
-                                               data-msg-required="Bu alan boş bırakılamaz."/>
-                                        <div className="photo-upload-widget" data-toggle="img-content">
-                                            {croppedImageUrl === "" ?
-                                                <div data-toggle="cropedImg" className="img" style={{backgroundImage: `url(${this.state.user.img})`}}></div>
-                                                : <div data-toggle="cropedImg" className="img" style={{backgroundImage: `url(${croppedImageUrl})`}}></div>
-                                            }
-                                            <div className="upload-content" data-toggle="triggerUpload">
-                                                <i className="icon icon-add-picture"></i>
-                                                <span className="text">Fotoğraf Yükle</span>
-                                            </div>
-                                        </div>
-
-                                    </label>
-                                </div>
-                                <button type="submit" className="btn btn-primary">Kaydet</button>
+                            <PhotoModal imgSave={this.imgSave} />
                             </form>
                         </div>
                         <div className="col-lg-9 col-24">
@@ -160,33 +42,6 @@ class Profile extends Component {
                         </div>
                     </div>
                 </div>
-
-                <Modal className="photo-modal" data-toggle="photo-modal" show={this.state.photoModalStatus}
-                       onHide={this.handleClose} centered>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        {src && (
-                            <ReactCrop
-                                src={src}
-                                crop={crop}
-                                ruleOfThirds
-                                onImageLoaded={this.onImageLoaded}
-                                onChange={this.onCropChange}
-                            />
-                        )}
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={this.handleClose}>
-                            İptal
-                        </Button>
-                        <Button variant="primary" onClick={this.saveCrop}>
-                            Kaydet
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-
             </Layout>
         );
     }
